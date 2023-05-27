13f9a24b1bc4f92eaa38b56601a890b85b038e5b
Yubeen <ybhs020308@naver.com>
2023-05-27 22:44:30 +0900

    23-05-27

---
+++ SecurityPro/src/main/java/com/example/Webpro/SecurityProApplication.java
@@ -0,0 +1,13 @@
+package com.example.Webpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityProApplication.class, args);
+	}
+
+}
